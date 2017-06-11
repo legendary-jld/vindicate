7cb1db42365f67ae169c1c5c0b5eec84265fa053
Jeremy Downs <contact@devinairsoftware.com>
2017-06-10 23:04:16 -0400

    Finding valid items

--- js/vindicate.js
+++ js/vindicate.js
@@ -7,7 +7,23 @@
             soft: true,
             activeForm: false,
             submitTo: "",
-            requiredMessage: "This is a required field."
+            requiredMessage: "This is a required field.",
+            parent: "form-group",
+            helper: "help-block",
+            validationStates: {
+              valid: {
+                icon: "fa fa-check",
+                color: "#0F0"
+              },
+              warning: {
+                icon: "fa fa-exclamation",
+                color: "orange"
+              },
+              invalid: {
+                icon:"fa fa-exclamation-circle",
+                color:"#A00"
+              }
+            },
             formatMessages: {
               date: "Please enter a valid date. (YYYY-MM-DD)",
               time: "Please enter a valid time (xx:xx)",
@@ -17,33 +33,23 @@
               decimal: "Please enter a decimal value (xxx.xx)",
               url: "Please enter a valid url. (www.website.com/example)",
               phone7: "Please enter a 7 digit phone number. (xxx-xxxx)",
-              phone10: "Please enter a 10 digit phone number. (xxx-xxx-xxxx)"
+              phone10: "Please enter a 10 digit phone number. (xxx-xxx-xxxx)",
               creditcard: "Please enter a valid credit card number."
             }
-            parent: "form-group"
-            helper: "help-block"
-            validationStates: {
-              valid: {
-                icon: "fa fa-check",
-                color: "#0F0"
-                },
-              warning: {
-                icon: "fa fa-exclamation",
-                color: "orange"
-                },
-              invalid: {
-                icon:"fa fa-exclamation-circle",
-                color:"#A00"
-                }
-              }
         }, options );
 
-        // Greenify the collection based on the settings variable.
-        return this.css({
-            color: settings.color,
-            backgroundColor: settings.backgroundColor
+        return this.each(function() {
+          var $form_this = $(this);
+          // console.log("$form_this", $form_this);
+          $form_this.find(":input").each(function() {
+            var $input_this = $(this);
+            // console.log("$input_this", $input_this);
+            if ($input_this.attr('data-vindicate')) {
+              var input_data = $input_this.data("vindicate").split("|");
+              console.log("input_data", input_data);
+            }
+          })
         });
-
     };
 
 }( jQuery ));
